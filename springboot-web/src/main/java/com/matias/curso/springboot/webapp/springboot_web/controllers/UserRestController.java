279a40ab5343d28f475181437f1432e101c374fe
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-02-02 09:50:28 -0300

    clase RequestMapping

--- springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -4,13 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/rest/details")
+    @GetMapping("/details")
     public Map<String,Object> details() {
         Map<String,Object> response = new HashMap<>();
         response.put("title", "Página de detalles");
