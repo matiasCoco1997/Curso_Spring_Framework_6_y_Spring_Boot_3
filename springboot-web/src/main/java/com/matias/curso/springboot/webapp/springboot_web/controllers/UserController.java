880d18a01dfbb662dc1abe1d21c0722a37e599e3
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-02-02 10:04:03 -0300

    Creación model "User"

--- springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -4,16 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.matias.curso.springboot.webapp.springboot_web.modals.User;
+
 
 @Controller
 public class UserController {
 
     @GetMapping("/app/details")
     public String details(Model model) {
-
+        User user = new User("Matías", "Coco");
         model.addAttribute("title", "Página de detalles");
-        model.addAttribute("name", "Matías");
-        model.addAttribute("lastname", "Coco");
+        model.addAttribute("user", user);
         return "details";
     }
 }
--- springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.matias.curso.springboot.webapp.springboot_web.modals.User;
+
 
 @RestController
 @RequestMapping("/api")
@@ -15,9 +17,9 @@ public class UserRestController {
     @GetMapping("/details")
     public Map<String,Object> details() {
         Map<String,Object> response = new HashMap<>();
+        User user = new User("Matías", "Coco");
         response.put("title", "Página de detalles");
-        response.put("name", "Matías");
-        response.put("lastname", "Coco");
+        response.put("user", user);
         return response;
     }
 }
