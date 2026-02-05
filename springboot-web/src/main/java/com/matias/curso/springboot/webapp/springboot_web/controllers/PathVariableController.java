ca6bfbd7d5e836143c0a4fe524bdd78ba7e05209
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-02-05 15:53:00 -0300

    Pathvariable
    compartir desde la url información con información variable

--- springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,22 +1,24 @@
 package com.matias.curso.springboot.webapp.springboot_web.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matias.curso.springboot.webapp.springboot_web.modals.dto.ParamDto;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
-@RequestMapping("/api/params")
-public class RequestParamsController {
+@RequestMapping("/api/var")
+public class PathVariableController {
 
-    @GetMapping("/foo")
-    public ParamDto foo(@RequestParam String message) {
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
         ParamDto param = new ParamDto();
+
         param.setMessage(message);
+
         return param;
     }
     
