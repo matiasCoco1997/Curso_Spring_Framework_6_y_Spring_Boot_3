1c056a89eafa8128f2f100a1e66a851c5a88f64f
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-02-18 11:05:09 -0300

    inyeccion de valores por @Values

--- springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -8,24 +8,39 @@ import com.matias.curso.springboot.webapp.springboot_web.modals.dto.ParamDto;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
 
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.listOfValue}")
+    private String[] listOfValue;
+
+    @Value("${config.code}")
+    private Integer code;
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
+
         ParamDto param = new ParamDto();
-
         param.setMessage(message);
-
         return param;
+
     }
 
     @GetMapping("/mix/{product}/{id}")
@@ -45,6 +60,18 @@ public class PathVariableController {
         return user;
     }
     
+    @GetMapping("/values")
+    public Map<String, Object> values() {
+        
+        Map<String, Object> json = new HashMap<>();
+        
+        json.put("username", username);
+        json.put("code", code);
+        json.put("listOfValue", listOfValue);
+        json.put("message", message);
+
+        return json;
+    }
     
     
 }
