99b0a6baf053054a45e23cc3346cb4a0ad58adca
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-02-18 16:32:30 -0300

    Creacion de archivos de configuracion separados

---
+++ springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/ValuesConfig.java
@@ -0,0 +1,13 @@
+package com.matias.curso.springboot.webapp.springboot_web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:values.properties")
+})
+public class ValuesConfig {
+
+}
