1537603dcc4fdd8d08fc0d710b5cb53a9442b256
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-02-19 15:07:52 -0300

    objeto environment de Spring

--- springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ springboot-web/src/main/java/com/matias/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -9,7 +9,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +43,9 @@ public class PathVariableController {
 
       @Value("#{ ${config.valuesMap} }")
     private Map<String, Object> valuesMap;
+
+    @Autowired
+    private Environment env;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -76,6 +81,8 @@ public class PathVariableController {
         json.put("username", username);
         json.put("code", code);
         json.put("message", message);
+        json.put("message2", env.getProperty("config.message", "No hay mensaje"));
+        json.put("code2", env.getProperty("config.code", Integer.class));
         json.put("listOfValue", listOfValue);
         json.put("valueList", valueList);
         json.put("valueString", valueString);
