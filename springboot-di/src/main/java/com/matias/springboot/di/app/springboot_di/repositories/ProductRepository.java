c02286e5403e99b5c88d6a8c6f8d73e8510aca91
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-02-20 11:42:28 -0300

    creacion de findById y findAll

---
+++ springboot-di/src/main/java/com/matias/springboot/di/app/springboot_di/repositories/ProductRepository.java
@@ -0,0 +1,30 @@
+package com.matias.springboot.di.app.springboot_di.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.matias.springboot.di.app.springboot_di.models.Product;
+
+public class ProductRepository {
+
+    private List<Product> data;
+
+    public ProductRepository() {
+        this.data = Arrays.asList(
+            new Product(1L,"Zapatillas Nike", 300L),
+            new Product(2L,"Remera Adidas", 400L),
+            new Product(3L,"Pantalon Puma", 500L) ,       
+            new Product(4L,"Gorra Umbro", 600L)       
+        );
+    }
+
+    public List<Product> findAll() {
+        return data;
+    }
+
+    public Product findById(Long id){
+        Product product = data.stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
+        return product;
+    }
+
+}
