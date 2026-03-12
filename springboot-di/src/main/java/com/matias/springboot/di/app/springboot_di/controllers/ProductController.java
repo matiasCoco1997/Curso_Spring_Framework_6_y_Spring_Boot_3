d52442ec67643022b24eb6595af573ff480ec9ea
Matias Ariel Coco <91424961+matiasCoco1997@users.noreply.github.com>
2026-03-12 15:08:45 -0300

    implementando autowired y component

--- springboot-di/src/main/java/com/matias/springboot/di/app/springboot_di/controllers/ProductController.java
+++ springboot-di/src/main/java/com/matias/springboot/di/app/springboot_di/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.matias.springboot.di.app.springboot_di.models.Product;
 import com.matias.springboot.di.app.springboot_di.services.ProductService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api")
 public class ProductController {
 
-    private ProductService productService = new ProductService();
+    @Autowired
+    private ProductService productService;
 
     @GetMapping  
     public List<Product> list(){
--- springboot-di/src/main/java/com/matias/springboot/di/app/springboot_di/services/ProductService.java
+++ springboot-di/src/main/java/com/matias/springboot/di/app/springboot_di/services/ProductService.java
@@ -2,18 +2,22 @@ package com.matias.springboot.di.app.springboot_di.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.matias.springboot.di.app.springboot_di.models.Product;
 import com.matias.springboot.di.app.springboot_di.repositories.ProductRepository;
 
+@Component
 public class ProductService implements IProductService {
 
-    private ProductRepository productRepository = new ProductRepository();
+    @Autowired
+    private ProductRepository productRepository;
 
     public List<Product> findAll(){
         return productRepository.findAll().stream().map(p -> {
             Double priceTax = p.getPrice() * 1.25d;
             //Product newProduct = new Product(p.getId(), p.getName(), priceTax.longValue());
-            //return newProduct;
             Product newProduct = (Product)p.clone();
             newProduct.setPrice(priceTax.longValue());
             return newProduct;
